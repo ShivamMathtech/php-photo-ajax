bdcfcfcaac3558ce422d236119d3c20831272f71
ShivamMathtech <drshivam49@gmail.com>
2024-11-12 20:48:50 +0530

    version 1.0.0

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File uploading with Ajax</title>
+</head>
+
+<body>
+    <h1>File uploading with ajax </h1>
+</body>
+
+</html>
